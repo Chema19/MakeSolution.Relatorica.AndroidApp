673373c87c2ad397d10cff547ed7232083c416ad
josemaria inga villafuerte <chemalonso96@gmail.com>
2019-09-24 08:02:07 -0500

    cambios de proyecto

---
+++ app/src/main/java/makesolution/relatorica/activities/MainActivity.kt
@@ -0,0 +1,54 @@
+package makesolution.relatorica.activities
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.view.MenuItem
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import makesolution.relatorica.R
+import makesolution.relatorica.fragments.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        return@OnNavigationItemSelectedListener navigateTo(item)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener (mOnNavigationItemSelectedListener)
+        navigation.selectedItemId = R.id.navigation_home
+
+    }
+    private fun fragmentFor(item: MenuItem) : Fragment {
+        when(item.itemId){
+            R.id.navigation_home -> {
+                return HomeFragment()
+            }
+            R.id.navigation_childs -> {
+                return ChildFragment()
+            }
+            R.id.navigation_my_books -> {
+                return BookFragment()
+            }
+            R.id.navigation_store -> {
+                return StoreFragment()
+            }
+            R.id.navigation_profile -> {
+                return ProfileFragment()
+            }
+        }
+        return HomeFragment()
+    }
+    private fun navigateTo(item: MenuItem): Boolean{
+        item.setChecked(true)
+        return supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.content, fragmentFor(item))
+            .commit() > 0
+
+    }
+}
---
+++ app/src/main/java/makesolution/relatorica/fragments/ProfileFragment.kt
@@ -0,0 +1,26 @@
+package makesolution.relatorica.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import makesolution.relatorica.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+
+}
