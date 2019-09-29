79316f0f531fde7f0d5c74add2e2f18e068d91e4
jamac1 <jmac3112@gmail.com>
2019-09-29 02:13:11 -0500

    home

---
+++ app/src/main/java/makesolution/relatorica/activities/HomeTutorialActivity.kt
@@ -0,0 +1,13 @@
+package makesolution.relatorica.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import makesolution.relatorica.R
+
+class HomeTutorialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_tutorial)
+    }
+}
