538cf46d2d284896450ea6628578857665624361
jamac1 <jmac3112@gmail.com>
2019-09-29 02:28:11 -0500

    load youtube

--- app/src/main/java/makesolution/relatorica/activities/HomeTutorialActivity.kt
+++ app/src/main/java/makesolution/relatorica/activities/HomeTutorialActivity.kt
@@ -2,12 +2,20 @@ package makesolution.relatorica.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.youtube.player.YouTubeStandalonePlayer
+import kotlinx.android.synthetic.main.activity_home_tutorial.*
 import makesolution.relatorica.R
 
 class HomeTutorialActivity : AppCompatActivity() {
 
+    val APIKEY ="AIzaSyDGgAf4Mq6KFcQwdYAzJn8wfWg3D-b6YAY"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_tutorial)
+        btn_load.setOnClickListener({
+            val intent = YouTubeStandalonePlayer.createVideoIntent(this,APIKEY,"SsBjJ3o7K04")
+            startActivity(intent)
+        })
+
     }
 }
