503bde4156469dcce9d24fc8785a3d1e42c28580
Martin Ng <u201520759@upc.edu.pe>
2019-11-21 02:11:55 -0500

    Pop up Compra

--- app/src/main/java/makesolution/relatorica/activities/HistoryActivity.kt
+++ app/src/main/java/makesolution/relatorica/activities/HistoryActivity.kt
@@ -1,12 +1,16 @@
 package makesolution.relatorica.activities
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.androidnetworking.error.ANError
 import kotlinx.android.synthetic.main.content_store.*
+import kotlinx.android.synthetic.main.dialog_purchase.*
 import makesolution.relatorica.R
 import makesolution.relatorica.models.StoreModel
 import makesolution.relatorica.networks.RelatoricaApi
@@ -51,7 +55,12 @@ class HistoryActivity : AppCompatActivity() {
             Log.d("Respuesta Falsa", historyResponse!!.Message)
             return
         }
-        Toast.makeText(this, "Comprra realizada satisfactoriamente.", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "Comprra realizada satisfactoriamente.", Toast.LENGTH_SHORT).show()
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_purchase)
+        dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
         val intento = Intent(this, MainActivity::class.java)
         startActivity(intento)
     }
